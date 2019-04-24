06f13ffc36ec2aeb0e07bbd9f8572af219fdb0c5
Jessica Mallian <jlmallian@gmail.com>
2019-04-23 19:23:08 -0500

    it's not much, but it helped me understand delegation better

--- Day8/CreateNumbersViewController.swift
+++ Day8/CreateNumbersViewController.swift
@@ -6,12 +6,24 @@
 //  Copyright © 2019 Jessica Mallian. All rights reserved.
 //
 
-import UIKit 
+import UIKit
+
+protocol CreatedRandomNumberDelegate {
+    func didCreate(number: Int) -> Bool
+}
 
 class CreateNumbersViewController: UIViewController {
     @IBOutlet var displayLabel: UILabel!
+    var createdRandomNumberDelegate: CreatedRandomNumberDelegate!
     
     @IBAction func addNumberButtonPressed(_ sender: Any) {
+        let randomInt = Int.random(in: 1..<11)
+        let numberAdded = createdRandomNumberDelegate.didCreate(number: randomInt)
+        if numberAdded {
+            displayLabel.text = "Number \(randomInt) was added."
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
     @IBAction func goBackButtonPressed(_ sender: Any) {
--- Day8/DisplayNumbersViewController.swift
+++ Day8/DisplayNumbersViewController.swift
@@ -11,17 +11,45 @@ import UIKit
 class DisplayNumbersViewController: UIViewController {
 
     @IBOutlet var displayLabel: UILabel!
-    
-    
+    var theNumbers = [Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if theNumbers.count > 0 {
+            createDisplay()
+        }
+    }
+    
+    private func createDisplay() {
+        var displayString = "Your numbers are "
+        for number in theNumbers {
+            displayString += " \(number)"
+        }
+        displayString += "."
+        displayLabel.text = displayString
+    }
 
     @IBAction func goToNumbersButtonPressed(_ sender: Any) {
         let vc = storyboard?.instantiateViewController(withIdentifier: "CreateNumbersViewController") as! CreateNumbersViewController
+        vc.createdRandomNumberDelegate = self
         present(vc, animated: true, completion: nil)
+    }
+}
+
+extension DisplayNumbersViewController: CreatedRandomNumberDelegate {
+    
+    func didCreate(number: Int) -> Bool {
+        if theNumbers.contains(number) {
+            return false
+        } else {
+            theNumbers.append(number)
+            return true
+        }
     }
 }
 
